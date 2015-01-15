0a0118c7643ef0cecde6f4999cbbd862d5800d43
Colin Xia <charleshsia@163.com>
2015-01-16 00:24:07 +0800

    add pickling

---
+++ 10_io_programming/03_ pickling.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Python提供两个模块来实现序列化：cPickle和pickle，用的时候，先尝试导入cPickle，如果失败，再导入pickle
+try:
+    import c
+except Exception, e:
+    raise
+else:
+    pass
+finally:
+    pass
